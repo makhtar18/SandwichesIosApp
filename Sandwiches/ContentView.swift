b66b41792c12be67f1291ebab4358e554fd44427
makhtar18 <122763616+makhtar18@users.noreply.github.com>
2023-04-26 13:03:57 -0700

    Initial Commit

---
+++ Sandwiches/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sandwiches
+//
+//  Created by Mehvish Akhtar on 26/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sandwiches/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Sandwiches
+//
+//  Created by Mehvish Akhtar on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
